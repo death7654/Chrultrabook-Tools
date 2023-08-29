8c74cd2c8e82f4cb9e6217c1af286251e90a086d
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2023-08-28 22:51:37 -0500

    Fix flashing issue

--- app/script.js
+++ app/script.js
@@ -5,6 +5,30 @@ window.addEventListener("DOMContentLoaded", () => {
           sidebar = body.querySelector("nav");
           sidebarToggle = body.querySelector(".sidebar-toggle");
     
+    function loadPage(path) {
+        if (window.currentFrame.src.endsWith(path)) return;
+        const newFrame = document.createElement("iframe");
+        newFrame.classList.add("view_frame");
+        newFrame.addEventListener("load", () => {
+            newFrame.style.display = "";
+            window.currentFrame.remove();
+            window.currentFrame = newFrame;
+        })
+        newFrame.style.display = "none";
+        newFrame.src = path;
+        window.currentFrame.parentElement.insertBefore(newFrame, window.currentFrame);
+    }
+    
+    document.getElementById("home").addEventListener("click", () => {
+        loadPage("Dashboard/index.html");
+    })
+    document.getElementById("fan").addEventListener("click", () => {
+        loadPage("Fan Control/fancontrol.html");
+    })
+    document.getElementById("diagnostics").addEventListener("click", () => {
+        loadPage("cbmem/cbmem.html");
+    })
+    
     let getMode = localStorage.getItem("mode");
     if (getMode && getMode ==="dark") {
         body.classList.toggle("dark");
