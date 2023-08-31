e47e964a58b9200261417678a36e6cfbee463634
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-08-31 11:31:07 -0400

    Revert "code cleanup and refactoring (WIP)"

--- app/ectools/setFanAuto.js
+++ app/ectools/setFanAuto.js
@@ -1,13 +1,14 @@
 const exec = require("child_process").exec;
+var child;
 
-function setFanAuto() {
-  exec('"C:\\Program Files\\crosec\\ectool" autofanctrl',
-    function fanSpeed(error, stdout) {
-      return fanSpeed;
-    });
-}
+function setFanAuto(){
+    child = exec('"C:\\Program Files\\crosec\\ectool" autofanctrl',
+      function fanSpeed(error, stdout) {
+        return fanSpeed;
+      });
+    }
 
-module.exports = { setFanAuto }
+    module.exports = {setFanAuto}
 
 
 
