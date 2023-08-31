31570f12e6b91ab3106f6c607098233364ac13dd
ninelore <ninelore@protonmail.com>
2023-08-31 16:44:14 +0200

    code cleanup pt1

--- app/ectools/setFanMaxSpeed.js
+++ app/ectools/setFanMaxSpeed.js
@@ -1,14 +1,13 @@
 const exec = require("child_process").exec;
-var child;
 
-function setFanSpeedMax(){
-    child = exec('"C:\\Program Files\\crosec\\ectool" fanduty 100',
-      function fanSpeed(error, stdout) {
-        return fanSpeed;
-      });
-    }
+function setFanSpeedMax() {
+  exec('"C:\\Program Files\\crosec\\ectool" fanduty 100',
+    function fanSpeed(error, stdout) {
+      return fanSpeed;
+    });
+}
 
-    module.exports = {setFanSpeedMax}
+module.exports = { setFanSpeedMax }
 
 
 
