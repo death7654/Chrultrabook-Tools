b135d1504deba79146f14f9ce71d701d61c98763
Robinson Arysseril <robinsongeorgearysseril301@gmail.com>
2023-09-17 09:11:47 -0400

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   src-tauri/.gitignore 	new file:   src-tauri/Cargo.lock 	new file:   src-tauri/Cargo.toml 	new file:   src-tauri/build.rs 	new file:   src-tauri/icons/128x128.png 	new file:   src-tauri/icons/128x128@2x.png 	new file:   src-tauri/icons/32x32.png 	new file:   src-tauri/icons/Square107x107Logo.png 	new file:   src-tauri/icons/Square142x142Logo.png 	new file:   src-tauri/icons/Square150x150Logo.png 	new file:   src-tauri/icons/Square284x284Logo.png 	new file:   src-tauri/icons/Square30x30Logo.png 	new file:   src-tauri/icons/Square310x310Logo.png 	new file:   src-tauri/icons/Square44x44Logo.png 	new file:   src-tauri/icons/Square71x71Logo.png 	new file:   src-tauri/icons/Square89x89Logo.png 	new file:   src-tauri/icons/StoreLogo.png 	new file:   src-tauri/icons/icon.icns 	new file:   src-tauri/icons/icon.ico 	new file:   src-tauri/icons/icon.png 	new file:   src-tauri/src/dev.doc 	new file:   src-tauri/src/ectool 	new file:   src-tauri/src/main.rs 	new file:   src-tauri/tauri.conf.json 	new file:   src/assets/Icons/app/close.svg 	new file:   src/assets/Icons/app/icon.ico 	new file:   src/assets/Icons/app/minimize.svg 	new file:   src/assets/Icons/cbmem/bugcbmemdash.svg 	new file:   src/assets/Icons/dashboard/cpu.svg 	new file:   src/assets/Icons/dashboard/ram.svg 	new file:   src/assets/Icons/dashboard/tachometer.svg 	new file:   src/assets/Icons/dashboard/thermometer.svg 	new file:   src/assets/Icons/fancontrol/animatedfan.svg 	new file:   src/assets/Icons/fancontrol/fancontroldash.svg 	new file:   src/assets/Icons/fancontrol/thermometer(white).svg 	new file:   src/assets/Icons/sidenavbar/Diagnostics.svg 	new file:   src/assets/Icons/sidenavbar/chrome.svg 	new file:   src/assets/Icons/sidenavbar/fan.svg 	new file:   src/assets/Icons/sidenavbar/home.svg 	new file:   src/assets/Icons/sidenavbar/info.svg 	new file:   src/assets/javascript.svg 	new file:   src/assets/tauri.svg 	new file:   src/index.html 	new file:   src/main.js 	new file:   src/styles.css

---
+++ src/main.js
@@ -0,0 +1,188 @@
+const { invoke } = window.__TAURI__.tauri;
+const { appWindow } = window.__TAURI__.window;
+
+//check for os type and hides things incompaitable
+setTimeout(async () => {
+  const os = await invoke("get_os");
+  //hides items not compatiable with linux
+  if (os !== "Windows") {
+    document.getElementById("noLinux").style.display = "none";
+    document.getElementById("noLinux2").style.display = "none";
+    document.getElementById("noLinux3").style.display = "none";
+    document.getElementById("biosVersion").style.display = "none";
+  }
+
+}, 1000);
+
+document
+  .getElementById("minimize")
+  .addEventListener("click", () => appWindow.minimize());
+
+document
+  .getElementById("close")
+  .addEventListener("click", () => appWindow.close());
+
+//homepage
+setInterval(async () => {
+  const ramUsage = await invoke("get_ram_usage");
+  const cpuUsage = await invoke("get_cpu_usage");
+  document.getElementById("ramPercentage").innerText = ramUsage + "%";
+  document.getElementById("cpuLoad").innerText = cpuUsage + "%";
+
+  //other custom info (ectools)
+  const stdout = await invoke("get_fan_rpm");
+  const cpuTempFunction = await invoke("get_cpu_temp");
+  let sensors = 0;
+  let temps = 0;
+  cpuTempFunction.split("\n").forEach((line) => {
+    const num = line.split("C)")[0].trim().split(" ").pop().trim();
+    if (num && !isNaN(num)) {
+      temps += parseFloat(num);
+      sensors++;
+    }
+  });
+  const averageTemp = temps / sensors;
+  document.getElementById("cpuTemp").innerText = averageTemp.toFixed(0) + "°C";
+
+  //sends information to fancontrol.html
+  const fanSpeed = stdout.toString().split(":").pop().trim();
+  document.getElementById("fanSpeed").innerText = fanSpeed + " RPM";
+  document.getElementById("cpuTempFan").innerText =
+    averageTemp.toFixed(0) + "°C";
+}, 1000);
+
+setTimeout(async () => {
+  const hostname = await invoke("get_hostname");
+  const bioslong = await invoke("get_bios_version");
+  const boardnamelong = await invoke("get_board_name");
+  const coreslong = await invoke("get_cpu_cores");
+  const cpunamelong = await invoke("get_cpu_name");
+
+  function systeminfodatatransfer() {
+    const bios = bioslong.split("\n")[1];
+    const boardname = boardnamelong.split("\n")[1];
+    const cores = coreslong.split("\n")[1];
+    const cpuname = cpunamelong.split("\n")[1];
+
+    document.getElementById("biosVersion").innerText = "Bios Version: " + bios;
+    document.getElementById("boardname").innerText = "Boardname: " + boardname;
+    document.getElementById("coreCPU").innerText = "Cores: " + cores + " Cores";
+    document.getElementById("hostname").innerText = "Hostname: " + hostname;
+    document.getElementById("cpuName").innerText = "CPU: " + cpuname;
+  }
+  systeminfodatatransfer();
+}, 1500);
+//setFanSpeeds
+
+var autoFan = document.getElementById("fanAuto");
+var offFan = document.getElementById("fanOff");
+var maxFan = document.getElementById("fanMax");
+
+function fanMax() {
+  invoke("set_fan_max");
+  autoFan.classList.remove("activeButton");
+  offFan.classList.remove("activeButton");
+  maxFan.classList.add("activeButton");
+}
+function fanOff() {
+  invoke("set_fan_off");
+  autoFan.classList.remove("activeButton");
+  offFan.classList.add("activeButton");
+  maxFan.classList.remove("activeButton");
+}
+function fanAuto() {
+  invoke("set_fan_auto");
+
+  autoFan.classList.add("activeButton");
+  offFan.classList.remove("activeButton");
+  maxFan.classList.remove("activeButton");
+}
+
+const buttonfanMax = document.getElementById("fanMax");
+buttonfanMax.addEventListener("click", () => fanMax());
+
+const buttonfanOff = document.getElementById("fanOff");
+buttonfanOff.addEventListener("click", () => fanOff());
+
+const buttonfanAuto = document.getElementById("fanAuto");
+buttonfanAuto.addEventListener("click", () => fanAuto());
+
+//system infopage
+const selected = document.querySelector(".selected");
+
+//sends info from ec to html
+function getSystemInfo() {
+  if (selected.innerText == "Boot Timestamps") {
+    setTimeout(async () => {
+      const cbmemdata = await invoke("get_cbmem");
+      document.getElementById("cbMemInfo").innerText = cbmemdata;
+    }, 1000);
+  } else if (selected.innerText == "Coreboot Log") {
+    setTimeout(async () => {
+      const coreboot = await invoke("get_coreboot");
+      document.getElementById("cbMemInfo").innerText = coreboot;
+    }, 1000);
+  } else if (selected.innerText == "Coreboot Extended Log") {
+    setTimeout(async () => {
+      const corebootlong = await invoke("get_coreboot_long");
+      document.getElementById("cbMemInfo").innerText = corebootlong;
+    }, 1000);
+  } else if (selected.innerText == "EC Console Log") {
+    setTimeout(async () => {
+      const console = await invoke("get_ec_console");
+      document.getElementById("cbMemInfo").innerText = console;
+    }, 1000);
+  } else if (selected.innerText == "Battery Info") {
+    setTimeout(async () => {
+      const battery = await invoke("get_battery");
+      document.getElementById("cbMemInfo").innerText = battery;
+    }, 1000);
+  } else if (selected.innerText == "EC Chip Info") {
+    setTimeout(async () => {
+      const flashChip = await invoke("get_flash_chip");
+      document.getElementById("cbMemInfo").innerText = flashChip;
+    }, 1000);
+  } else if (selected.innerText == "SPI Info") {
+    setTimeout(async () => {
+      const spi = await invoke("get_spi_info");
+      document.getElementById("cbMemInfo").innerText = spi;
+    }, 1000);
+  } else if (selected.innerText == "EC Protocol Info") {
+    setTimeout(async () => {
+      const protocol = await invoke("get_ec_protocol");
+      document.getElementById("cbMemInfo").innerText = protocol;
+    }, 1000);
+  } else if (selected.innerText == "Temp Sensor Info") {
+    setTimeout(async () => {
+      const temp_sensor = await invoke("get_temp_sensor");
+      document.getElementById("cbMemInfo").innerText = temp_sensor;
+    }, 1000);
+  } else if (selected.innerText == "Power Delivery Info") {
+    setTimeout(async () => {
+      const power_delivery = await invoke("get_power_delivery");
+      document.getElementById("cbMemInfo").innerText = power_delivery;
+    }, 1000);
+  } else {
+    document.getElementById("cbMemInfo").innerText = "Select Something";
+  }
+}
+
+//copy
+const buttoncbMem = document.getElementById("cbMem");
+buttoncbMem.addEventListener("click", () => getSystemInfo());
+
+function copyTxt(htmlElement) {
+  if (!htmlElement) return;
+
+  let elementText = htmlElement.innerText;
+
+  let inputElement = document.createElement("input");
+  inputElement.setAttribute("value", elementText);
+  document.body.appendChild(inputElement);
+  inputElement.select();
+  document.execCommand("copy");
+  inputElement.parentElement.removeChild(inputElement);
+}
+document.querySelector("#copyButton").addEventListener("click", () => {
+  copyTxt(document.querySelector("#cbMemInfo"));
+});
