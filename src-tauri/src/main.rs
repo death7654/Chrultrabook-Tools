4eb6fdc6246be2af832038284f4d9446e9cd469a
WeirdTreeThing <bradyn127@protonmail.com>
2023-09-23 08:54:19 -0400

    Fix cpu temp on linux

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -80,11 +80,11 @@ async fn get_cpu_temp() -> Option<String> {
     #[cfg(target_os = "linux")]
     {
         cmd = std::process::Command::new("cat")
-            .args(["/sys/class/thermal/thermal_zone*/temp"])
+            .args(["/sys/class/thermal/thermal_zone0/temp"]) // this is just a placeholder until a proper detction system is in place
             .output();
-        return Some(String::from(
-            match_result(cmd).parse::<i16>().unwrap() / 1000,
-        ));
+        return Some(
+            (match_result(cmd).parse::<i32>().unwrap() / 1000).to_string()
+        );
     }
 
     #[cfg(windows)]
