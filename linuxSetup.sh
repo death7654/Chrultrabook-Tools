5363cfc7fe11533cecb20c48a7d115ff6f8d5541
Robinson Arysseril <robinsongeorgearysseril301@gmail.com>
2023-09-29 16:49:52 +0530

    new file:   linuxSetup.sh

---
+++ linuxSetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+echo "Starting Setup Process"
+echo "creating directory"
+mkdir -p /usr/local/bin/CrosEC
+echo "created"
+echo "downloading tools"
+cd /usr/local/bin/CrosEC
+curl "https://tree123.org/chrultrabook/utils/ectool"
+curl "https://tree123.org/chrultrabook/utils/cbmem"
+echo "setting up tools"
+sudo chmod u+s /usr/local/bin/CrosEC/ectool
+sudo chmod u+s /usr/local/bin/CrosEC/cbmem
+export PATH=/usr/local/bin/CrosEC/ectool:$PATH
+export PATH=/usr/local/bin/CrosEC/cbmem:$PATH
+source ~/.bashrc
+echo "setup done"
+
+
