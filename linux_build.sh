710fca01eff7687addebd2a6249ef3c245b99cac
AHQ Softwares <ahqsecret@gmail.com>
2023-10-01 22:50:39 +0530

    Update linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash 
-
-# build as normal first
-npm run tauri build
-
 ROOT=$(pwd)
 APPIMAGEDIR="$ROOT/src-tauri/target/release/bundle/appimage"
 APPDIR="$(find $APPIMAGEDIR -name "chrultrabook-controller*.AppDir")"
