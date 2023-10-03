2bb09398a7f67080b40803e98c89ee85de95a12e
Robinson Arysseril <robinsongeorgearysseril301@gmail.com>
2023-10-03 12:03:14 +0530

    modified:   linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -8,12 +8,12 @@ APPIMAGE="$(find $APPIMAGEDIR -name "chrultrabook-controller*.AppImage")"
 APPIMAGETOOLURL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
 
 # modify appimage to add our stuff
-sudo rm $APPIMAGE
+rm $APPIMAGE
 cd $APPIMAGEDIR
-sudo cp $ROOT/linux/AppRun $APPDIR
+cp $ROOT/linux/AppRun $APPDIR
 curl -L https://tree123.org/chrultrabook/utils/ectool -o $APPDIR/usr/bin/ectool
 curl -L https://tree123.org/chrultrabook/utils/cbmem -o $APPDIR/usr/bin/cbmem
-sudo chmod +x $APPDIR/usr/bin/*
+chmod +x $APPDIR/usr/bin/*
 curl -L $APPIMAGETOOLURL -o AppImageTool.AppImage
-sudo chmod +x AppImageTool.AppImage
+chmod +x AppImageTool.AppImage
 ARCH=x86_64 ./AppImageTool.AppImage $APPDIR $APPIMAGE
