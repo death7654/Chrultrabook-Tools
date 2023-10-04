a18b624a9106a8a7bc82907cc513532482db3c99
WeirdTreeThing <bradyn127@protonmail.com>
2023-10-04 06:46:29 -0400

    Remove Vayne check on activity light

--- src/main.js
+++ src/main.js
@@ -126,7 +126,7 @@ setTimeout(async () => {
   document.getElementById("cpuName").innerText = "CPU: " + cpuname;
 /*
   //shows or hides activity light settings based on boardname (only shows to Candy and Kefka)
-  if(boardname !== "Candy" && boardname !== "Kefka" && boardname !== "Vayne")
+  if(boardname !== "Candy" && boardname !== "Kefka" && boardname)
   {
     document.getElementById("ActivityLight").style.display = "none";
     document.getElementById("keyboardBacklight").classList.add("afterCheck");
