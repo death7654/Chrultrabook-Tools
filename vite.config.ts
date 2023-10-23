d2c8d37e0608df65f9e9c541bb1df3969141c1d6
ninelore <ninelore@protonmail.com>
2023-10-23 03:56:55 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig(async () => ({
   plugins: [vue()],
 
