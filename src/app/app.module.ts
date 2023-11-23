525f717363d9b960def0ebd7926ae592f95271b9
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-11-23 22:09:31 +0530

    fix accidental deletion 	modified:   src/app/app.module.ts 	modified:   src/app/sidenav/sidenav.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,11 @@ import { FancontrolComponent } from './fancontrol/fancontrol.component';
 import { SystemControlsComponent } from './system-controls/system-controls.component';
 import { SystemInformationComponent } from './system-information/system-information.component';
 import { SettingsComponent } from './settings/settings.component';
-import { SidenavComponent } from "./sidenav/sidenav.component";
 
 @NgModule({
   declarations: [AppComponent, SidenavComponent, DashboardComponent, FancontrolComponent, SystemControlsComponent, SystemInformationComponent, SettingsComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent], 
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
