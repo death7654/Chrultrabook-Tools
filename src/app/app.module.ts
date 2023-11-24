d1be48f4965f9e80532de144e8e6bda17df992a2
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-11-24 18:37:14 +0530

    Revert "fixes"
    This reverts commit ac36b68559b8618268721992f0d4e55bce1472b6.
    
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FancontrolComponent } from './fancontrol/fancontrol.component';
 import { SystemControlsComponent } from './system-controls/system-controls.component';
 import { SystemInformationComponent } from './system-information/system-information.component';
 import { SettingsComponent } from './settings/settings.component';
-import { SidenavComponent } from "./sidenav/sidenav.component";
 
 @NgModule({
   declarations: [AppComponent, SidenavComponent, DashboardComponent, FancontrolComponent, SystemControlsComponent, SystemInformationComponent, SettingsComponent],
