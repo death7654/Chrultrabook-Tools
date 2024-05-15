77f973870b5033b6107a42c83ea0a96f1874d1c3
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-05-15 13:52:06 +0530

    external windows width and height made adjustable 	modified:   src-tauri/src/main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,37 +2,37 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-async fn new_window(handle: &tauri::AppHandle, label: &str, angular_path: &str) -> tauri::Window<tauri::Wry> {
+async fn new_window(handle: &tauri::AppHandle, label: &str, angular_path: &str, width: f64, height: f64) -> tauri::Window<tauri::Wry> {
   return tauri::WindowBuilder::new(
     handle,
     label, /* the unique window label */
     tauri::WindowUrl::App(angular_path.parse().unwrap())
-  ).build().unwrap();
+  ).inner_size(width, height).build().unwrap();
 }
 
 #[tauri::command]
 async fn open_custom_fan(handle: tauri::AppHandle)
 {
-  new_window(&handle, "fan", "custom_fan").await;
+  new_window(&handle, "fan", "custom_fan", 500.0, 300.0).await;
 }
 
 #[tauri::command]
 async fn open_keyboard_extra(handle: tauri::AppHandle)
 {
-  new_window(&handle, "keyboard", "keyboard_extra").await;
+  new_window(&handle, "keyboard", "keyboard_extra", 500.0, 300.0).await;
 }
 
 
 #[tauri::command]
 async fn open_diagnostics(handle: tauri::AppHandle)
 {
-  new_window(&handle, "diagnostics", "diagnostics").await;
+  new_window(&handle, "diagnostics", "diagnostics", 500.0, 300.0).await;
 }
 
 #[tauri::command]
 async fn open_settings(handle: tauri::AppHandle)
 {
-  new_window(&handle, "settings", "settings").await;
+  new_window(&handle, "settings", "settings", 500.0, 325.0).await;
 }
 
 fn main() {
