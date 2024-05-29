a5c97771464de9b698b470bfc3abb1bb925cc284
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-05-29 17:51:41 +0530

    converted open_window function to rust module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,43 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-async fn new_window(
-    handle: &tauri::AppHandle,
-    label: &str,
-    angular_path: &str,
-    width: f64,
-    height: f64,
-) -> tauri::WebviewWindow<tauri::Wry> {
-    return tauri::WebviewWindowBuilder::new(
-        handle,
-        label, /* the unique window label */
-        tauri::WebviewUrl::App(angular_path.parse().unwrap()),
-    )
-    .inner_size(width, height)
-    .resizable(false)
-    .build()
-    .unwrap();
-}
+mod open_window;
 
 #[tauri::command]
 async fn open_custom_fan(handle: tauri::AppHandle) {
-    new_window(&handle, "fan", "custom_fan", 600.0, 450.0).await;
+    open_window::new_window(&handle, "fan", "custom_fan", 600.0, 450.0).await;
 }
 
 #[tauri::command]
 async fn open_keyboard_extra(handle: tauri::AppHandle) {
-    new_window(&handle, "keyboard", "keyboard_extra", 500.0, 300.0).await;
+    open_window::new_window(&handle, "keyboard", "keyboard_extra", 500.0, 300.0).await;
 }
 
 #[tauri::command]
 async fn open_diagnostics(handle: tauri::AppHandle) {
-    new_window(&handle, "diagnostics", "diagnostics", 600.0, 420.0).await;
+    open_window::new_window(&handle, "diagnostics", "diagnostics", 600.0, 420.0).await;
 }
 
 #[tauri::command]
 async fn open_settings(handle: tauri::AppHandle) {
-    new_window(&handle, "settings", "settings", 500.0, 350.0).await;
+    open_window::new_window(&handle, "settings", "settings", 500.0, 350.0).await;
 }
 
 fn main() {
--- src-tauri/src/open_window.rs
+++ src-tauri/src/open_window.rs
@@ -1,14 +1,14 @@
-async fn new_window(
+pub async fn new_window(
     handle: &tauri::AppHandle,
     label: &str,
     angular_path: &str,
     width: f64,
     height: f64,
-) -> tauri::Window<tauri::Wry> {
-    return tauri::WindowBuilder::new(
+) -> tauri::WebviewWindow<tauri::Wry> {
+    return tauri::WebviewWindowBuilder::new(
         handle,
         label, /* the unique window label */
-        tauri::WindowUrl::App(angular_path.parse().unwrap()),
+        tauri::WebviewUrl::App(angular_path.parse().unwrap()),
     )
     .inner_size(width, height)
     .resizable(false)
