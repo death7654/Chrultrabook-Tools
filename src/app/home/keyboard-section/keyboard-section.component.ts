b6eda278f7b89ec3f419ddbd21ff456ad96639ec
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-06-04 17:53:28 +0530

    Hooked section to backend and added checks to see if backlight exists

--- src/app/home/keyboard-section/keyboard-section.component.ts
+++ src/app/home/keyboard-section/keyboard-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonComponent } from '../../button/button.component';
 import { invoke } from "@tauri-apps/api/core"
 
@@ -11,12 +11,31 @@ import { invoke } from "@tauri-apps/api/core"
   styleUrl: './keyboard-section.component.scss'
 })
 export class KeyboardSectionComponent {
-
   backlight_percentage: string = 'N/A'
+  percentage: number = 0
+  backlight_exists: boolean = !true;
+  disabled_class: string = ''
+  async ngOnInit() {
+    console.log('hello')
+    let output: string = await invoke("execute", { program: "ectool", arguments: ['pwmgetkblight'], reply: true });
+    let split = output.split(" ");
+    if (split[0] !== "Current")
+      {
+        this.disabled_class = 'disabled'
+        this.backlight_exists = !false
+      }
+      else
+      {
+        this.backlight_percentage = split[4]
+        this.percentage = Number(split[4])
+      }
+  }
+
   update_percentage(event: MouseEvent)
   {
     this.backlight_percentage = (event.target as HTMLInputElement).value + "%"
   }
+
 
   keyboard_more(){
     console.log('more');
