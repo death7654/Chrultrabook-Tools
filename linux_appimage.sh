a6e9857e5683c95e64304fcd520efe31dcdeb220
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-06-17 21:24:18 +0530

    add post install script

---
+++ linux_appimage.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash 
+ROOT=$(pwd)
+
+if [[ -d $ROOT/target ]]; then
+    APPIMAGEDIR="$ROOT/target/release/bundle/appimage"
+elif [[ -d $ROOT/src-tauri/target ]]; then
+    APPIMAGEDIR="$ROOT/src-tauri/target/release/bundle/appimage"
+else
+    echo "Error: target folder not found. Aborting" 
+    exit 1
+fi
+
+APPDIR="$(find $APPIMAGEDIR -name "chrultrabook-tools*.AppDir")"
+APPIMAGE="$(find $APPIMAGEDIR -name "chrultrabook-tools*.AppImage")"
+APPIMAGETOOLURL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+
+# modify appimage to add our stuff
+rm $APPIMAGE
+cd $APPIMAGEDIR
+cp $ROOT/linux/AppRun $APPDIR
+curl -L https://tree123.org/chrultrabook/utils/ectool -o $APPDIR/usr/bin/ectool
+curl -L https://tree123.org/chrultrabook/utils/cbmem -o $APPDIR/usr/bin/cbmem
+chmod +x $APPDIR/usr/bin/*
+curl -L $APPIMAGETOOLURL -o AppImageTool.AppImage
+chmod +x AppImageTool.AppImage
+ARCH=x86_64 ./AppImageTool.AppImage $APPDIR $APPIMAGE
