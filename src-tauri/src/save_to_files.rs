3e472bb5190ec2f573f558089a2355d8640cfd15
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-06-27 13:35:14 +0530

    fix app crash upon closing window to save file

--- src-tauri/src/save_to_files.rs
+++ src-tauri/src/save_to_files.rs
@@ -7,7 +7,11 @@ pub fn select_path(app: tauri::AppHandle, file_name: String, content: String) {
         .set_file_name(file_name)
         .add_filter("File Extension", &["txt"])
         .save_file(|file_path| {
-            let file = file_path.unwrap();
-            fs::write(&file, content).expect("unable to write")
+            match file_path {
+                Some(ref _out) => {
+                    let file = file_path.unwrap();
+                    fs::write(&file, content).expect("unable to write")},
+                None => {},
+            }
         });
 }
