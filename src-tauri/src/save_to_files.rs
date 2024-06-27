64159ef6c32262c538d12d666940279d2095bd12
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-06-27 18:59:19 +0530

    fix app crashing upon user not selecting a path when they save files

--- src-tauri/src/save_to_files.rs
+++ src-tauri/src/save_to_files.rs
@@ -6,12 +6,11 @@ pub fn select_path(app: tauri::AppHandle, file_name: String, content: String) {
         .file()
         .set_file_name(file_name)
         .add_filter("File Extension", &["txt"])
-        .save_file(|file_path| {
-            match file_path {
-                Some(ref _out) => {
-                    let file = file_path.unwrap();
-                    fs::write(&file, content).expect("unable to write")},
-                None => {},
+        .save_file(|file_path| match file_path {
+            Some(ref _out) => {
+                let file = file_path.unwrap();
+                fs::write(&file, content).expect("unable to write")
             }
+            None => {}
         });
 }
