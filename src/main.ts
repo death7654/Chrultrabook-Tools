5a58c224188144e5804ee4b6839b441d04297c07
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-06-29 19:50:08 +0530

    prevent right clicks

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 
+//prevent right clicks
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
