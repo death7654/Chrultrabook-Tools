c352f30a11b2ad7a7941e8b1d6bf1d0c0fcaa8dc
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2024-10-12 18:29:12 +0530

    added missing binary error handling

--- src/app/diagnostics/diagnostics.component.ts
+++ src/app/diagnostics/diagnostics.component.ts
@@ -6,28 +6,60 @@ import { invoke } from "@tauri-apps/api/core";
   standalone: true,
   imports: [],
   templateUrl: "./diagnostics.component.html",
-  styleUrl: "./diagnostics.component.scss",
+  styleUrl: "./diagnostics.component.scss"
 })
 export class DiagnosticsComponent implements OnInit {
   collected_info: string = "";
   selected_function: string = "";
   disabled: boolean = false;
+  ectools: boolean = false;
 
   ngOnInit() {
-    invoke("os").then((os) => {
+    invoke("os").then(os => {
       if (os == "macOS") {
         this.disabled = true;
+      }
+    });
+    invoke("execute", {
+      program: "ectool",
+      arguments: ["hello"],
+      reply: true
+    }).then((event: any) => {
+      let output = event.toLowerCase().trim();
+      console.log(output);
+      if (output !== "ec says hello!") {
+        this.ectools = true;
+        this.collected_info = "You are missing the ECTOOL Binary";
+      }
+    });
+
+    invoke("execute", {
+      program: "cbmem",
+      arguments: ["-v"],
+      reply: true
+    }).then((event: any) => {
+      let output = event
+        .toLowerCase()
+        .trim()
+        .split(" ");
+      if (output[0] !== "cbmem") {
+        this.disabled = true;
+        if (this.ectools == true) {
+          this.collected_info = "You are missing ECTOOL and CBMEM Binaries";
+        } else {
+          this.collected_info = "You are missing the CBMEM Binary";
+        }
       }
     });
   }
 
   async select() {
-    this.selected_function = (
-      document.getElementById("diagnostic_dropdown") as HTMLInputElement
-    ).value;
+    this.selected_function = (document.getElementById(
+      "diagnostic_dropdown"
+    ) as HTMLInputElement).value;
     if (this.selected_function !== "Select") {
       this.collected_info = await invoke("diagnostics", {
-        selected: this.selected_function,
+        selected: this.selected_function
       });
     }
   }
@@ -39,7 +71,7 @@ export class DiagnosticsComponent implements OnInit {
   save() {
     invoke("save", {
       filename: this.selected_function,
-      content: this.collected_info,
+      content: this.collected_info
     });
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { FanSectionComponent } from "./fan-section/fan-section.component";
 import { KeyboardSectionComponent } from "./keyboard-section/keyboard-section.component";
 import { ActivityLightSectionComponent } from "./activity-light-section/activity-light-section.component";
 import { ExtraSectionComponent } from "./extra-section/extra-section.component";
+import { invoke } from "@tauri-apps/api/core";
 
 @Component({
   selector: "app-home",
@@ -16,4 +17,26 @@ import { ExtraSectionComponent } from "./extra-section/extra-section.component";
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  class: string = " "
+
+  ngOnInit()
+  {
+    setTimeout(() => {
+      invoke("execute", {
+        program: "ectool",
+        arguments: ["hello"],
+        reply: true,
+      }).then((event: any) =>
+      {
+        let output = event.toLowerCase().trim();
+        if (output == "ec says hello!")
+        {
+          this.class = "displaynone"
+        }
+      });
+    })
+  }
+
+}
