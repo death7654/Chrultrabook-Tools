ef301691909b2e5451d7758a1930371dbedfcbff
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-06-09 22:43:17 +0530

    update to angular 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
     selector: "app-root",
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.scss"
 })
