6e4935c91a490810deb7085ff77899b4813f9437
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-06-17 17:16:26 +0530

    formating

--- src/app/custom-fan/fan-curves/fan-curves.component.ts
+++ src/app/custom-fan/fan-curves/fan-curves.component.ts
@@ -8,10 +8,10 @@ import DragData from "chartjs-plugin-dragdata";
 import { invoke } from "@tauri-apps/api/core";
 
 @Component({
-    selector: "app-fan-curves",
-    imports: [BaseChartDirective],
-    templateUrl: "./fan-curves.component.html",
-    styleUrl: "./fan-curves.component.scss"
+  selector: "app-fan-curves",
+  imports: [BaseChartDirective],
+  templateUrl: "./fan-curves.component.html",
+  styleUrl: "./fan-curves.component.scss"
 })
 export class FanCurvesComponent implements OnInit {
   mode_value: string = " ";
